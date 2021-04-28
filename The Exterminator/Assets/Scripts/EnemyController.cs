51d18d5a55250ac039159f14e2142f932b6c5d50
calebxcaleb <calebxcaleb@gmail.com>
2021-04-28 12:12:03 -0400

    Fixed poison collision and enemy sprite order
    Added proper physics collision for poison particles and changed the order of enemy sprites so that animations will be possible.

--- The Exterminator/Assets/Scripts/EnemyController.cs
+++ The Exterminator/Assets/Scripts/EnemyController.cs
@@ -14,22 +14,25 @@ public class EnemyController : MonoBehaviour
     private Rigidbody2D rb;
     private float force = 15.0f;
     private bool air = false;
-    private bool particle = false;
+    private bool poison = false;
+    private bool airParticle = false;
+    private bool poisonParticle = false;
     private Hashtable sizes = new Hashtable();
 
 
     void Start()
     {
-        sizes.Add(1, new float[]{0.25f, 4f});
-        sizes.Add(2, new float[]{0.5f, 6f});
-        sizes.Add(3, new float[]{1f, 8f});
-        sizes.Add(4, new float[]{1.5f, 10f});
-        sizes.Add(5, new float[]{2f, 12f});
+        sizes.Add(1, new float[]{0.25f, 2f});
+        sizes.Add(2, new float[]{0.5f, 4f});
+        sizes.Add(3, new float[]{1f, 6f});
+        sizes.Add(4, new float[]{1.5f, 8f});
+        sizes.Add(5, new float[]{2f, 10f});
         
         this.transform.localScale = new Vector3(((float[])sizes[size])[0], ((float[])sizes[size])[0], 1);
         this.GetComponent<Rigidbody2D>().mass = ((float[])sizes[size])[1];
-        for(int i = 0; i < this.transform.childCount; i++) {
-            GameObject leg = this.transform.GetChild(i).gameObject;
+        GameObject Body = this.transform.GetChild(0).gameObject;
+        for(int i = 0; i < Body.transform.childCount; i++) {
+            GameObject leg = Body.transform.GetChild(i).gameObject;
             if (leg.tag == "leg"){
                 leg.GetComponent<Rigidbody2D>().mass = ((float[])sizes[size])[1];
             }
@@ -45,20 +48,43 @@ public class EnemyController : MonoBehaviour
         MoveEnemy();
     }
 
+	void explode(){
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(this.transform.position, 5f);
+        foreach (Collider2D hit in colliders) {
+            Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+            if (rb != null){
+		        var dir = rb.transform.position - this.transform.position;
+				rb.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		        rb.AddForce(dir.normalized * 200.0f * rb.mass);
+			}
+		}
+	}
+
     void MoveEnemy(){
-        if (particle){
+        if (airParticle){
             Vector3 dir3 = player.GetComponent<ShootingController>().dir;
             Vector2 dir2 = new Vector2(dir3.x * force, dir3.y * force);
             rb.AddForce(dir2);
         }
+        if (poisonParticle){
+            Destroy(this.GetComponent<Animator>());
+            changeSpriteLayer();
+            this.transform.GetChild(0).transform.DetachChildren();
+            explode();
+            this.transform.gameObject.layer = 10;
+            this.GetComponent<LayEgg>().enabled = true;
+            this.GetComponent<DisableObject>().enabled = true;
+            Destroy(this.GetComponent<EnemyController>());
+        }
     }
 
     void changeSpriteLayer(){
-        this.GetComponent<SpriteRenderer>().sortingOrder = 0;
-        for(int i = 0; i < this.transform.childCount; i++) {
-            GameObject leg = this.transform.GetChild(i).gameObject;
-            if (leg.tag == "leg"){
-                leg.GetComponentInChildren<SpriteRenderer>().sortingOrder = 0;
+        GameObject Body = this.transform.GetChild(0).gameObject;
+        Body.GetComponent<SpriteRenderer>().sortingOrder = 0;
+        for(int i = 0; i < Body.transform.childCount; i++) {
+            GameObject bodyPart = Body.transform.GetChild(i).gameObject;
+            if (bodyPart.tag == "leg"){
+                bodyPart.GetComponentInChildren<SpriteRenderer>().sortingOrder = 0;
             }
         }
     }
@@ -70,13 +96,7 @@ public class EnemyController : MonoBehaviour
             }
 
             if (player.GetComponent<ShootingController>().poison){
-                Destroy(this.GetComponent<Animator>());
-                changeSpriteLayer();
-                this.transform.DetachChildren();
-                this.transform.gameObject.layer = 10;
-                this.GetComponent<LayEgg>().enabled = true;
-                this.GetComponent<DisableObject>().enabled = true;
-                Destroy(this.GetComponent<EnemyController>());
+                poison = true;
             }
 
         }
@@ -85,13 +105,18 @@ public class EnemyController : MonoBehaviour
     void OnTriggerExit2D(Collider2D col){
         if (col.tag == "shoot"){
             air = false;
-            particle = false;
+            poison = false;
+            airParticle = false;
+            poisonParticle = false;
         }
     }
 
     void OnParticleCollision(GameObject other){
         if (air){
-            particle = true;
+            airParticle = true;
+        }
+        if(poison){
+            poisonParticle = true;
         }
     }
 }
